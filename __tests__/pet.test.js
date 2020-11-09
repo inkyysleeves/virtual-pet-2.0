167beedff86254807407c0442b205efc1dbdbe6a
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2020-11-09 15:35:42 +0000

    added a few methods to my pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,80 @@
-const Pet = require('../src/pet.js');
+const Pet = require("../src/pet.js");
 
-describe('constructor', () => {
-    it('returns an object', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(Object);
+describe("constructor", () => {
+    it("returns an object", () => {
+        expect(new Pet("Fido")).toBeInstanceOf(Object);
+    });
+    it("sets the name property", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.name).toEqual("Fido");
+    });
+    it("has an initial age of 0", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.age).toEqual(0);
+    });
+    it("has an intial hunger of 0", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.hunger).toBe(0);
+    });
+    it("has an initial fitness of 10", () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.fitness).toEqual(10);
+    });
+});
+
+describe("growUp", () => {
+    it("increases the age by 1", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    });
+    it("increases the hunger by 5", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+    it("decrease the fitness by 3", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    });
+});
+
+describe("walk", () => {
+    it("increase fitness by 4", () => {
+        const pet = new Pet("Fido");
+
+        pet.fitness = 4;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(8);
+    });
+    it("increases fitness to a maximum of 10", () => {
+        const pet = new Pet("Fido");
+
+        pet.fitness = 8;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(10);
+    });
+});
+describe('feed', () => {
+    it('should decrease the pets hunger by 3', () => {
+        const pet = new Pet('Fido');
+
+        pet.hunger = 3;
+        pet.feed();
+
+        expect(pet.hunger).toBe(0);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,32 @@
-function Pet() {}
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.hunger = MINIMUM_HUNGER;
+    this.fitness = MAXIMUM_FITNESS;
+}
+
+Pet.prototype.growUp = function() {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+}
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = 10;
+    }
+}
+Pet.prototype.feed = function() {
+    if ((this.hunger -= 3) <= MINIMUM_HUNGER) {
+        this.hunger = MINIMUM_HUNGER;
+    } else {
+        this.hunger -= 3;
+    }
+}
 
 module.exports = Pet;
