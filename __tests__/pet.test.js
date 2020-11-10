4253f0616349ea1a24d581a1563aa3ac104f7681
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2020-11-10 19:36:00 +0000

    finished the virtual pet, dont have the time to do the extra steps

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -78,4 +78,90 @@ describe('feed', () => {
 
         expect(pet.hunger).toBe(0);
     });
+    it('throws an error if the pet is no longer alive', () => {
+        const pet = new Pet('Fido');
+
+        pet.age = 30;
+        expect(() => pet.feed()).toThrow("Your pet is no longer with us . . . soz!!");
+    });
+});
+describe('checkUp', () => {
+    it('should check if the pet needs a walk', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 2;
+        pet.checkUp();
+        expect(pet.checkUp()).toEqual("I need a walk");
+    });
+    it('should check if the pet is hungry or not', () => {
+        const pet = new Pet('Fido');
+
+        pet.hunger = 6;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I am hungry");
+    });
+    it('it should check if the pet needs a walk and is hungry', () => {
+        const pet = new Pet('Fido');
+
+        pet.hunger = 6;
+        pet.fitness = 2;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I am hungry AND I need a walk");
+    });
+    it('checks if it doeesnt need either', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 7;
+        pet.hunger = 4;
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual("I feel great!");
+    });
+});
+describe('isAlive', () => {
+    it('check if the pets fitness is 0 or less', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 0;
+        expect(pet.isAlive()).toEqual(false);
+    });
+    it('check if the pets hunger is 10 or more', () => {
+        const pet = new Pet('Fido');
+
+        pet.hunger = 10;
+        pet.isAlive();
+
+        expect(pet.isAlive()).toEqual(false);
+    });
+    it('check if the pets age is 30 or more', () => {
+        const pet = new Pet('Fido');
+
+        pet.age = 30;
+        pet.isAlive();
+
+        expect(pet.isAlive()).toEqual(false);
+    });
+    it('checks if the others are not false then it should be true', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 3;
+        pet.hunger = 4;
+        pet.age = 25;
+        pet.isAlive();
+
+        expect(pet.isAlive()).toEqual(true);
+    });
+    it('checks if the pet is alive or not', () => {
+        const pet = new Pet('fido');
+
+        pet.age = 30;
+        pet.fitness = 0;
+        pet.hunger = -1;
+
+        pet.checkUp();
+
+        expect(pet.checkUp()).toEqual('Your pet is no longer with us . . . soz!!');
+    });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,7 @@
 const MAXIMUM_FITNESS = 10;
 const MINIMUM_HUNGER = 0;
+const MAXIMUM_AGE = 30;
+const DEATH_RATTLE = 'Your pet is no longer with us . . . soz!!';
 
 function Pet(name) {
     this.name = name;
@@ -7,25 +9,67 @@ function Pet(name) {
     this.hunger = MINIMUM_HUNGER;
     this.fitness = MAXIMUM_FITNESS;
 }
+Pet.prototype = {
+    get isAlive() {
+        return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+    },
+    growUp: function() {
+        if (!this.isAlive()) {
+            throw new Error(DEATH_RATTLE);
+        }
+        this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
+    },
 
-Pet.prototype.growUp = function() {
-    this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
-}
-
-Pet.prototype.walk = function() {
-    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
-        this.fitness += 4;
-    } else {
-        this.fitness = 10;
-    }
-}
-Pet.prototype.feed = function() {
-    if ((this.hunger - 3) <= MINIMUM_HUNGER) {
-        this.hunger = MINIMUM_HUNGER;
-    } else {
-        this.hunger -= 3;
+    walk: function() {
+        if (!this.isAlive()) {
+            throw new Error(DEATH_RATTLE);
+        }
+        if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+            this.fitness += 4;
+        } else {
+            this.fitness = 10;
+        }
+    },
+    feed: function() {
+        if (!this.isAlive()) {
+            throw new Error(DEATH_RATTLE);
+        }
+        if ((this.hunger - 3) <= MINIMUM_HUNGER) {
+            this.hunger = MINIMUM_HUNGER;
+        } else {
+            this.hunger -= 3;
+        }
+    },
+    checkUp: function() {
+        if (this.age >= MAXIMUM_AGE || this.hunger < MINIMUM_HUNGER || this.fitness <= 0) {
+            return DEATH_RATTLE;
+        }
+        if (this.fitness <= 3 && this.hunger >= 5) {
+            return 'I am hungry AND I need a walk';
+        }
+        if (this.fitness <= 3) {
+            return 'I need a walk';
+        }
+        if (this.hunger >= 5) {
+            return 'I am hungry';
+        } else {
+            return 'I feel great!';
+        }
+    },
+    isAlive: function() {
+        if (this.fitness <= 0) {
+            return false;
+        }
+        if (this.hunger >= 10) {
+            return false;
+        }
+        if (this.age >= 30) {
+            return false;
+        } else {
+            return true
+        }
     }
 }
 
